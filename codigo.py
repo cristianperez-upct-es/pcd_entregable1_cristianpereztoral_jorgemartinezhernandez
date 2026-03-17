82302dd127ed65581448d1e35576992e0d8020af
jorgeMartinez293 <jorgediy@gmail.com>
2026-03-17 11:38:32 +0100

    uniendo todo en un solo archivo (prueba y código)

--- codigo.py
+++ codigo.py
@@ -94,3 +94,30 @@ class Repuesto:
 
     def __str__(self):
         return f'Nombre: {self.nombre} | Proveedor: {self.proveedor} |  Precio: {self.precio}'
+    
+if __name__ == '__main__':
+    
+    bolt = Repuesto('bolt', 'paco', 10)
+    wing = Repuesto('wing', 'francisco', 50432)
+
+    milenialfalcon = NaveEstelar('1', 1, 'milenial_falcon',{bolt:3, wing:1}, ['Lonely Han', 'Chewbacca'], 1, Clase.EJECUTOR)
+    deathstar = EstacionEspacial('2', 2, 'death_star', {bolt: 100}, ['Darth Vader', 'Death Moul', 'Palpatin'], 2, 'espacio')
+    xwing = CazaEstelar('3', 3, 'x-wing', {wing:2}, 3)
+
+    droiddepot = Almacen('Droid Depot', {bolt: 100, wing:200}, 'Tatooine')
+
+    imperio = Imperio('imperio estelar', [deathstar, xwing], [droiddepot])
+
+    # Imperio
+    print(imperio)
+    print(imperio.get_unidades())
+    print(imperio.get_almacenes())
+    imperio.set_unidad(xwing)
+    print(imperio.get_unidades())
+
+    # NaveEstelar / Almacén
+    print(milenialfalcon.get_info())
+    print(droiddepot.consultar_stock())
+    milenialfalcon.adquirir_repuesto(droiddepot, bolt, 2)
+    print(milenialfalcon.get_info())
+    print(droiddepot.consultar_stock())
